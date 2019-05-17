4479c75203a0052d4cc6c083a899a6998768b208
andrej <navikom11@mail.ru>
2019-05-17 18:33:18 +0300

    added lazy

--- src/layouts/Admin.jsx
+++ src/layouts/Admin.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React from "react";
+import React, { Suspense } from "react";
 import PropTypes from "prop-types";
 import { Switch, Route, Redirect } from "react-router-dom";
 // creates a beautiful scrollbar
@@ -7,11 +7,14 @@ import PerfectScrollbar from "perfect-scrollbar";
 import "perfect-scrollbar/css/perfect-scrollbar.css";
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
+// utils
+import { lazy } from "utils";
+
 // core components
-import Navbar from "components/Navbars/Navbar.jsx";
-import Footer from "components/Footer/Footer.jsx";
-import Sidebar from "components/Sidebar/Sidebar.jsx";
-import FixedPlugin from "components/FixedPlugin/FixedPlugin.jsx";
+const Navbar = lazy(() => import("components/Navbars/Navbar.jsx"));
+const Footer = lazy(() => import("components/Footer/Footer.jsx"));
+const Sidebar = lazy(() => import("components/Sidebar/Sidebar.jsx"));
+const FixedPlugin = lazy(() => import("components/FixedPlugin/FixedPlugin.jsx"));
 
 // core containers
 import ScrollContainer from "containers/ScrollContainer/ScrollContainer.jsx";
@@ -50,81 +53,90 @@ class Dashboard extends React.Component {
       mobileOpen: false
     };
   }
+
   handleImageClick = image => {
-    this.setState({ image: image });
+    this.setState({image: image});
   };
   handleColorClick = color => {
-    this.setState({ color: color });
+    this.setState({color: color});
   };
   handleFixedClick = () => {
     if (this.state.fixedClasses === "dropdown") {
-      this.setState({ fixedClasses: "dropdown show" });
+      this.setState({fixedClasses: "dropdown show"});
     } else {
-      this.setState({ fixedClasses: "dropdown" });
+      this.setState({fixedClasses: "dropdown"});
     }
   };
   handleDrawerToggle = () => {
-    this.setState({ mobileOpen: !this.state.mobileOpen });
+    this.setState({mobileOpen: !this.state.mobileOpen});
   };
+
   getRoute() {
     return this.props.location.pathname !== "/admin/maps";
   }
+
   resizeFunction = () => {
     if (window.innerWidth >= 960) {
-      this.setState({ mobileOpen: false });
+      this.setState({mobileOpen: false});
     }
   };
+
   componentDidMount() {
     window.addEventListener("resize", this.resizeFunction);
   }
+
   componentDidUpdate(e) {
     if (e.history.location.pathname !== e.location.pathname) {
       if (this.state.mobileOpen) {
-        this.setState({ mobileOpen: false });
+        this.setState({mobileOpen: false});
       }
     }
   }
+
   componentWillUnmount() {
     window.removeEventListener("resize", this.resizeFunction);
   }
+
   render() {
-    const { classes, ...rest } = this.props;
+    const {classes, ...rest} = this.props;
     return (
       <div className={classes.wrapper}>
-        <Sidebar
-          routes={routes}
-          logoText={"WebInSolut"}
-          logo={logo}
-          image={this.state.image}
-          handleDrawerToggle={this.handleDrawerToggle}
-          open={this.state.mobileOpen}
-          color={this.state.color}
-          {...rest}
-        />
-        <ScrollContainer>
-          <Navbar
+        <Suspense fallback={"Loading..."}>
+          <Sidebar
             routes={routes}
+            logoText={"WebInSolut"}
+            logo={logo}
+            image={this.state.image}
             handleDrawerToggle={this.handleDrawerToggle}
+            open={this.state.mobileOpen}
+            color={this.state.color}
             {...rest}
           />
-          {/* On the /maps route we want the map to be on full screen - this is not possible if the content and conatiner classes are present because they have some paddings which would make the map smaller */}
-          {this.getRoute() ? (
-            <div className={classes.content}>
-              <div className={classes.container}>{switchRoutes}</div>
-            </div>
-          ) : (
-            <div className={classes.map}>{switchRoutes}</div>
-          )}
-          {this.getRoute() ? <Footer /> : null}
-          <FixedPlugin
-            handleImageClick={this.handleImageClick}
-            handleColorClick={this.handleColorClick}
-            bgColor={this.state["color"]}
-            bgImage={this.state["image"]}
-            handleFixedClick={this.handleFixedClick}
-            fixedClasses={this.state.fixedClasses}
-          />
-        </ScrollContainer>
+          <ScrollContainer>
+            <Navbar
+              routes={routes}
+              handleDrawerToggle={this.handleDrawerToggle}
+              {...rest}
+            />
+            {/* On the /maps route we want the map to be on full screen - this is not possible if the content and conatiner classes are present because they have some paddings which would make the map smaller */}
+            {this.getRoute() ? (
+              <div className={classes.content}>
+                <div className={classes.container}>{switchRoutes}</div>
+              </div>
+            ) : (
+              <div className={classes.map}>{switchRoutes}</div>
+            )}
+            {this.getRoute() ? <Footer/> : null}
+            <FixedPlugin
+              handleImageClick={this.handleImageClick}
+              handleColorClick={this.handleColorClick}
+              bgColor={this.state["color"]}
+              bgImage={this.state["image"]}
+              handleFixedClick={this.handleFixedClick}
+              fixedClasses={this.state.fixedClasses}
+            />
+          </ScrollContainer>
+        </Suspense>
       </div>
     );
   }
